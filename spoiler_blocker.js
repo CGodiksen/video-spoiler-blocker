a2ceb1f84f008738f80aa8852516888de115c659
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-03-18 23:11:53 +0100

    Added containers for filters and standardized naming in html

--- spoiler_blocker.js
+++ spoiler_blocker.js
@@ -1,11 +1,13 @@
 if (window.location.href.includes("watch")) {
   const timeDisplay = document.getElementsByClassName("ytp-time-display notranslate")[0]
   
+  // Removing the video length from the player if the title is blocked.
   const videoTitle = document.getElementsByClassName("title style-scope ytd-video-primary-info-renderer")[0].textContent
   if (videoTitle.includes("Positive Mood") && timeDisplay) {
     timeDisplay.remove()
   }
   
+  // Removing the video length from the player if the channel is blocked.
   const channelName = document.getElementsByClassName("style-scope ytd-video-owner-renderer").namedItem("channel-name").innerText
   if (channelName === "Lounge Music" && timeDisplay) {
     timeDisplay.remove()
@@ -21,7 +23,7 @@ if (window.location.href.includes("watch")) {
     }
   }
   
-  // Removing the video length if the video title or channel name is blocked.
+  // Removing the video length on the thumbnail if the video title or channel name is blocked.
   videos.forEach(video => {
     const [thumbnail, details] = video.childNodes
     const videoLength = thumbnail.getElementsByTagName("ytd-thumbnail-overlay-time-status-renderer")[0];

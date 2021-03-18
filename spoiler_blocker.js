41d583c964b81616ccaae9a5955ea11751394602
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-03-18 10:54:02 +0100

    Only blocking the time display on a video if the user is currently watching a video.

--- spoiler_blocker.js
+++ spoiler_blocker.js
@@ -1,9 +1,12 @@
-var videoTitle = document.getElementsByClassName("title style-scope ytd-video-primary-info-renderer")[0]
-var timeDisplay = document.getElementsByClassName("ytp-time-display notranslate")[0]
+if (window.location.href.includes("watch")) {
+  console.log("Test");
+  var videoTitle = document.getElementsByClassName("title style-scope ytd-video-primary-info-renderer")[0]
+  var timeDisplay = document.getElementsByClassName("ytp-time-display notranslate")[0]
 
-console.log(videoTitle.textContent)
-console.log(timeDisplay);
+  console.log(videoTitle.textContent)
+  console.log(timeDisplay);
 
-if (videoTitle.textContent.includes("Sleep Instantly")) {
-  timeDisplay.remove()
+  if (videoTitle.textContent.includes("Positive Mood")) {
+    timeDisplay.remove()
+  }
 }

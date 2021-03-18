7493430efb359f2a5bbe680f7b4dd8cf572bd3f8
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-03-18 02:36:17 +0100

    Conditionally removing time display if video title includes blocked phrase.

--- spoiler_blocker.js
+++ spoiler_blocker.js
@@ -1,4 +1,9 @@
-var element = document.getElementsByClassName("ytp-time-display notranslate");
+var videoTitle = document.getElementsByClassName("title style-scope ytd-video-primary-info-renderer")[0]
+var timeDisplay = document.getElementsByClassName("ytp-time-display notranslate")[0]
 
-console.log(element);
-element[0].remove()
+console.log(videoTitle.textContent)
+console.log(timeDisplay);
+
+if (videoTitle.textContent.includes("Sleep Instantly")) {
+  timeDisplay.remove()
+}

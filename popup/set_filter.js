384bde792d5758999af04f535451554076b2ffe9
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2021-03-18 23:13:50 +0100

    Initialize elements from the popup html file

---
+++ popup/set_filter.js
@@ -0,0 +1,9 @@
+// Initialize elements from the popup html file.
+const inputChannelFilter = document.querySelector("div.popup-content input[name='channel-filter']");
+const inputTitleFilter= document.querySelector("div.popup-contnet input[name='title-fitler']");
+
+const channelFilterContainer = document.querySelector("div.channel-filter-container");
+const titleFilterContainer = document.querySelector(".div.title-filter-container")
+
+const addChannelFilterBtn = document.querySelector('.add-channel-filter');
+const addTitleFilterBtn = document.querySelector('.add-title-filter');
